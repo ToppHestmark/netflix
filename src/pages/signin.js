c63d3ccde6577472435aa14e8961ee5280b66cb7
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-10-18 17:16:24 +0200

    firebase in progress

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,18 +1,31 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
+import { FirebaseContext } from '../context/firebase'
 import { FooterContainer } from '../containers/footer';
 import { HeaderContainer } from '../containers/header';
 import { Form } from '../components'
 
 
 export default function SignIn() {
-  const [emailAddress, setEmailAddress] = useState();
-  const [password, setPassword] = useState();
-  const [error, setError] = useState("");
+  const { firebase } = useContext(FirebaseContext)
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
   const isInvalid = password === '' || emailAddress === '';
 
   const handleSignIn = (event) => {
     event.preventDefault();
+
+    firebase.auth()
+    .signInWithEmailAndPassword(emailAddress, password)
+    .then(() => {
+
+    })
+    .catch((error) => {
+      setEmailAddress('');
+      setPassword('')
+      setError(error.message)
+    })
   }
 
   return (
@@ -39,6 +52,13 @@ export default function SignIn() {
               Sign In
             </Form.Submit>
           </Form.Base>
+
+          <Form.Text>
+            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
+          </Form.TextSmall>
         </Form>
         
       </HeaderContainer>
