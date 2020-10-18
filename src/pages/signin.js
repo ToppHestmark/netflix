b32cd3947800a857b8a1de8efe55997cd4306374
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-10-18 16:58:24 +0200

    added sign in form

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,7 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { FooterContainer } from '../containers/footer';
+import { HeaderContainer } from '../containers/header';
+import { Form } from '../components'
 
-export default function Signin() {
+
+export default function SignIn() {
+  const [emailAddress, setEmailAddress] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState("");
+
+  const isInvalid = password === '' || emailAddress === '';
+
+  const handleSignIn = (event) => {
+    event.preventDefault();
+  }
+
   return (
-    <p>Sign-in page under construction</p>
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error> {error} </Form.Error>}
+
+          <Form.Base onSubmit={handleSignIn} method="POST">
+            <Form.Input
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)} 
+              />
+              <Form.Input
+              type="password"
+              autoComplete="off"
+              placeholder="Password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+              />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign In
+            </Form.Submit>
+          </Form.Base>
+        </Form>
+        
+      </HeaderContainer>
+      <FooterContainer />
+    </>
   )
 }
