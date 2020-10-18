b0b77c81670cece06916f44411b60f7769a71da5
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-10-18 18:10:22 +0200

    created a sign up page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,11 +1,14 @@
 import React, {useState, useContext} from 'react';
+import { useHistory } from 'react-router-dom';
 import { FirebaseContext } from '../context/firebase'
 import { FooterContainer } from '../containers/footer';
 import { HeaderContainer } from '../containers/header';
 import { Form } from '../components'
+import * as ROUTES from '../constants/routes';
 
 
 export default function SignIn() {
+  const history = useHistory()
   const { firebase } = useContext(FirebaseContext)
   const [emailAddress, setEmailAddress] = useState('');
   const [password, setPassword] = useState('');
@@ -19,7 +22,7 @@ export default function SignIn() {
     firebase.auth()
     .signInWithEmailAndPassword(emailAddress, password)
     .then(() => {
-
+      history.push(ROUTES.BROWSE);
     })
     .catch((error) => {
       setEmailAddress('');
@@ -57,7 +60,8 @@ export default function SignIn() {
             New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
           </Form.Text>
           <Form.TextSmall>
-            This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
+            This page is protected by Google reCAPTCHA to ensure you're not a bot. 
+            <Form.Link to="#">Learn more.</Form.Link>
           </Form.TextSmall>
         </Form>
         
