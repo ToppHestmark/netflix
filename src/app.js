e0224b09ff4ac34b4964359ae74894498a2de62d
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-10-19 18:48:38 +0200

    setting up router helper function

--- src/app.js
+++ src/app.js
@@ -1,23 +1,41 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Home, Browse, Signin, Signup } from './pages';
 import * as ROUTES from './constants/routes';
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
 
 function App() {
+  const user = null;
+
   return (
-    <Router>
-      <Route exact path="/browse">
-        <Browse />
-      </Route>
-      <Route exact path="/signin">
-        <Signin />
-      </Route>
-      <Route exact path="/signup">
-        <Signup />
-      </Route>
-      <Route exact path={ROUTES.HOME}>
-        <Home />
-      </Route>
+      <Router>
+        <Switch>
+
+          <IsUserRedirect 
+            user={user} 
+            loggedInPath={ROUTES.BROWSE} 
+            path={ROUTES.SIGN_IN}
+            exact 
+            >
+            <Signin />
+          </IsUserRedirect>
+          <IsUserRedirect 
+            user={user} 
+            loggedInPath={ROUTES.BROWSE} 
+            path={ROUTES.SIGN_UP}
+            exact 
+            >
+            <Signup />
+          </IsUserRedirect>
+
+        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+          <Browse />
+        </ProtectedRoute>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME} exact>
+          <Home />
+        </IsUserRedirect>
+
+      </Switch>
     </Router>
   );
 }
