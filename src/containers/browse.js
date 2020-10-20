e4e9897e1262b53d61f7984ad49050b9fedf6239
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-10-20 21:32:25 +0200

    finished off the Player

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState, useEffect } from 'react'
 import { SelectedProfileContainer } from './profiles';
 import { firebaseContext } from '../context/firebase';
-import { Loading, Header, Card } from '../components';
+import { Loading, Header, Card, Player } from '../components';
 import * as ROUTES from '../constants/routes';
 import logo from '../logo.svg';
 import { FooterContainer } from './footer';
@@ -99,11 +99,10 @@ export function BrowseContainer({ slides }) {
               ))}
             </Card.Entities>
             <Card.Feature category={category}>
-              {/* <Player>
+              <Player>
                 <Player.Button />
-                <Player.Video src="/video/bunny.mp4" />
-              </Player> */}
-              <p>hello</p>
+                <Player.Video src="/videos/bunny.mp4" />
+              </Player>
             </Card.Feature>
           </Card>
         ))}
